882481103227a357393ebbf93a36edcbbda77cfa
kymani anderson <kymanianderson@pop-os.localdomain>
2021-02-04 23:57:13 -0500

    bug fix

--- lab-2.py
+++ lab-2.py
@@ -4,22 +4,27 @@ import datetime
 app = Flask(__name__)
 dte = datetime.datetime.now()
 
-# fake Databases:
+# fake DBs:
 
 profileDB = {
+    "sucess": True,
+    "data": {
         "last_updated": "2/3/2021, 8:48:51 PM",
         "username": "coolname",
         "role": "Engineer",
         "color": "#3478ff"
     }
+}
 
-tankDB ={
-        "tankID": 1,
-        "tankLocDesc": "South-West",
-        "tankprcFull": 12,
-        "tankLatitude": 9,
-        "tankLongitude": "Master"
-    }
+tankDB =[
+    {
+	    "id": 1,
+        "location": "Engineering department",
+        "lat": 18.0051862,
+        "long": -76.7505108,
+        "percentage_full": 92
+    },
+]
 
 # Index
 @app.route("/", methods=["GET"])
@@ -31,19 +36,20 @@ def home():
 def profile():
     if request.method == "POST":
         # /POST
-        profileDB["last_updated"] = (dte.strftime("%c"))
-        profileDB["username"] = (request.json["username"])
-        profileDB["role"] = (request.json["role"])
-        profileDB["color"] = (request.json["color"])
+        profileDB["data"]["last_updated"] = (dte.strftime("%c"))
+        profileDB["data"]["username"] = (request.json["username"])
+        profileDB["data"]["role"] = (request.json["role"])
+        profileDB["data"]["color"] = (request.json["color"])
        
         return jsonify(profileDB)
    
     elif request.method == "PATCH":
         # /PATCH
-        profileDB["last_updated"].append(dte.strftime("%c"))
-        if request.json["username"] is not None: profileDB["username"] = (request.json["username"])
-        if request.json["role"] is not None: profileDB["role"] = (request.json["role"])
-        if request.json["color"] is not None: profileDB["color"] = (request.json["color"])
+        profileDB["data"]["last_updated"] = (dte.strftime("%c"))
+       
+        if request.json["username"] is not None: profileDB["data"]["username"] = (request.json["username"])
+        if request.json["role"] is not None: profileDB["data"]["role"] = (request.json["role"])
+        if request.json["color"] is not None: profileDB["data"]["color"] = (request.json["color"])
 
         return jsonify(profileDB)
 
